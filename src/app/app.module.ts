8bf93c7c4b9b7437b4d23f7cb752607f31c8f60b
Junyong <aszx6992@naver.com>
2018-04-02 17:20:54 +0900

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,47 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database-deprecated';
+import { AuthData } from '../providers/auth-data';
+import { Facebook } from '@ionic-native/facebook';
+import { GooglePlus } from '@ionic-native/google-plus';
+
+
+
+export const config = { 
+  apiKey: "AIzaSyDguP0NB-F-ogNoDM1J9XYhxyzhMOPqt4I",
+  authDomain: "template-6dc8c.firebaseapp.com",
+  databaseURL: "https://template-6dc8c.firebaseio.com",
+  projectId: "template-6dc8c",
+  storageBucket: "template-6dc8c.appspot.com",
+  messagingSenderId: "377019364393"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthData,
+    Facebook,
+    GooglePlus
   ]
 })
 export class AppModule {}
