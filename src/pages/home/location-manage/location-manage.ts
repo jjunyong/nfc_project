18555d8d0690a8ce85332783e573e4a248778213
Seoyoung LEE <homopod777@gamil.com>
2018-07-17 14:05:33 +0900

    location page modified - funtion trigger

--- src/pages/home/location-manage/location-manage.ts
+++ src/pages/home/location-manage/location-manage.ts
@@ -1,21 +1,21 @@
 
 import firebase from 'firebase';
-import {IonicPage } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular';
 import { Component } from '@angular/core';
-import { NavController, LoadingController} from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular/components/alert/alert-controller';
-import { AngularFirestore,  AngularFirestoreCollection } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { NavParams } from 'ionic-angular/navigation/nav-params';
 import { GlobalVars } from '../../../providers/global';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner'
 
 
 
-interface Item{
+interface Item {
   location1: any;
   location2: any;
   model: string;
-  quantity : number;
+  quantity: number;
 }
 
 @IonicPage()
@@ -26,101 +26,116 @@ interface Item{
 
 
 export class LocationManagePage {
-  private itemsCollection: AngularFirestoreCollection<Item>; 
-  itemList : any=[]; 
-  itemArray : any = [];
-  loadedItemList:  any=[]; 
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  itemList: any = [];
+  itemArray: any = [];
+  loadedItemList: any = [];
 
-  itemList1 : any=[];
-  itemList2 : any=[]; 
-  itemArray2 : any = [];
+  itemList1: any = [];
+  itemList2: any = [];
+  itemArray2: any = [];
 
 
-  items : any = [];
+  items: any = [];
   location: string;
-  itemgetList : any=[];
-  payload : string;
+  itemgetList: any = [];
+  payload: string;
   seperate: any[];
 
   location1: string;
   location2: string;
 
-  location_origin : any; 
+  location_origin: string;
 
-  constructor( public loadingCtrl: LoadingController,
+  constructor(public loadingCtrl: LoadingController,
     public alertCtrl: AlertController,
     public navCtrl: NavController,
     private afs: AngularFirestore,
-    public navParams : NavParams,
-    public global : GlobalVars
-  ){
-    
-    this.location_origin= this.navParams.get("location_origin")
-    console.log(this.location_origin, "from QR Page")
-    alert(this.location_origin);
-    // let alert = this.alertCtrl.create({
-    //   title : 'Location Origin',
-    //   subTitle : this.location_origin
-    // })
-    
-    this.payload = "null";
-
-    //this.payload = this.navParams.get('payload')
-    
-    this.payload = "A-5";
-    
-    if(this.payload!==null){
-
-      this.seperate = this.payload.split('-');
-      this.location1 = this.seperate[0];
-      this.location2 = this.seperate[1];
-      console.log(this.location1);
-      console.log(this.location2);
-      this.goTo2()
-    }
-    // alert(this.payload)
+    public navParams: NavParams,
+    public global: GlobalVars
+  ) {
 
 
 
-    let loadingPopup = this.loadingCtrl.create({
-      spinner: 'crescent', // icon style //
-      content: '',
-      duration: 1000
-    });
-    loadingPopup.present();
+    this.location_origin = null; //초기화
+    this.payload = null;
 
-    this.itemsCollection = afs.collection<Item>('item');
-    this.items= this.itemsCollection.valueChanges();
-    
-    this.items.subscribe((item)=>{
-      this.itemArray = item;
-      this.itemList = this.itemArray;
-      this.loadedItemList = this.itemArray;
-      loadingPopup.dismiss();
-    })
-    
+    this.payload = this.navParams.get('payload')
+    this.location_origin = this.navParams.get("location_origin")
 
-    this.location1 = "null";
-    this.location2 = "null";
-  }
+   console.log(this.location_origin, "from QR Page")
+
   
-ionViewWillEnter(){
-  // console.log('ionViewEnteredStockMangePage')
-  this.global.changeMessage(false);
-}
+      let loadingPopup = this.loadingCtrl.create({
+        spinner: 'crescent', // icon style //
+        content: '',
+        duration: 1000
+      });
+      loadingPopup.present();
+
+      this.itemsCollection = afs.collection<Item>('item');
+      this.items = this.itemsCollection.valueChanges();
+
+      this.items.subscribe((item) => {
+        this.itemArray = item;
+        this.itemList = this.itemArray;
+        this.loadedItemList = this.itemArray;
+        loadingPopup.dismiss();
+      })
+
+     
+    if (this.location_origin != null) {
+
+      this.afs.collection('location_map').doc(this.location_origin).valueChanges()
+        .subscribe((location_info: any) => {
+
+          this.location1 = location_info.location1;
+          this.location2 = location_info.location2;
+
+          console.log(this.location1);
+          console.log(this.location2);
+
+          this.goTo2();
+        })
+
+    }
+
+    else if (this.payload != null) {
+
+      this.afs.collection('location_map').doc(`${this.payload}`).valueChanges()
+        .subscribe((location_info: any) => {
+          this.location1 = location_info.location1;
+          this.location2 = location_info.location2;
+
+
+          console.log(this.location1);
+          console.log(this.location2);
+          this.goTo2()
+
+        })
+
+
+    }
+ 
+  }
+
+  ionViewWillEnter() {
+    // console.log('ionViewEnteredStockMangePage')
+    this.global.changeMessage(false);
+  }
 
 
   goTo1() {
 
-    if(this.location2==="null"){
-    this.itemsCollection = this.afs.collection<Item>('item', ref => ref.where('location1', '==', this.location1))
+    if (this.location2 === "null") {
+      this.itemsCollection = this.afs.collection<Item>('item', ref => ref.where('location1', '==', this.location1))
     } //location2 옵션 변경시 바뀌게 하는 조건
-    else{
-      this.itemsCollection = this.afs.collection<Item>('item', ref => ref.where('location1', '==', this.location1).where('location2','==',this.location2))
-    }  
+    else {
+      this.itemsCollection = this.afs.collection<Item>('item', ref => ref.where('location1', '==', this.location1).where('location2', '==', this.location2))
+    }
 
-    this.items= this.itemsCollection.valueChanges();
-     this.items.subscribe((item)=>{
+    this.items = this.itemsCollection.valueChanges();
+    this.items.subscribe((item) => {
       console.log(this.items)
       this.itemArray = item;
       this.itemList = this.itemArray;
@@ -130,38 +145,38 @@ ionViewWillEnter(){
   }
 
 
-    goTo2() {
-      if(this.location1==="null"){
-        this.itemsCollection = this.afs.collection<Item>('item', ref => ref.where('location2', '==', this.location2))
-        } //location2 옵션 변경시 바뀌게 하는 조건
-        else{
-          this.itemsCollection = this.afs.collection<Item>('item', ref => ref.where('location1', '==', this.location1).where('location2','==',this.location2))
-        }  
-    
-        this.items= this.itemsCollection.valueChanges();
-         this.items.subscribe((item)=>{
-          console.log(this.items)
-          this.itemArray = item;
-          this.itemList = this.itemArray;
-          this.loadedItemList = this.itemArray;
-    
-        })
-  }
-    
-  
-
-    openDetail(item){
-      this.navCtrl.push('ItemDetailPage',{
-        code : item.code,
-        name : item.name,
-        location1 : item.location1,
-        location2 : item.location2,
-        quantity : item.quantity
-      })
+  goTo2() {
+    if (this.location1 === "null") {
+      this.itemsCollection = this.afs.collection<Item>('item', ref => ref.where('location2', '==', this.location2))
+    } //location2 옵션 변경시 바뀌게 하는 조건
+    else {
+      this.itemsCollection = this.afs.collection<Item>('item', ref => ref.where('location1', '==', this.location1).where('location2', '==', this.location2))
     }
 
+    this.items = this.itemsCollection.valueChanges();
+    this.items.subscribe((item) => {
+      console.log(this.items)
+      this.itemArray = item;
+      this.itemList = this.itemArray;
+      this.loadedItemList = this.itemArray;
 
+    })
   }
+
+
+
+  openDetail(item) {
+    this.navCtrl.push('ItemDetailPage', {
+      code: item.code,
+      name: item.name,
+      location1: item.location1,
+      location2: item.location2,
+      quantity: item.quantity
+    })
+  }
+
+
+}
 
 
 
