3d062d1a81dd4939476dca67676847bf06273b31
homopod777@gmail.com <21300528@handong.edu>
2018-08-02 14:13:11 +0900

    topbar added

--- src/pages/repair/repair-notice/repair-notice.ts
+++ src/pages/repair/repair-notice/repair-notice.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFirestore } from 'angularfire2/firestore';
+import { GlobalVars } from '../../../providers/global';
 
 /**
  * Generated class for the RepairNoticePage page.
@@ -20,7 +21,7 @@ notice:any;
 backgroundImage = "https://firebasestorage.googleapis.com/v0/b/prototype-d68e4.appspot.com/o/%EB%A9%94%EC%9D%B8%ED%8E%98%EC%9D%B4%EC%A7%802_%ED%88%AC%EB%AA%85.png?alt=media&token=b4bb27d8-9ce6-44b5-b979-a5d24c2401b2";
 
 
-constructor(public navCtrl: NavController, public afs:AngularFirestore, public navParams: NavParams) {
+constructor(public navCtrl: NavController, public afs:AngularFirestore, public navParams: NavParams, public global: GlobalVars) {
   this.notice = this.navParams.get('notice');
 }
 
@@ -28,5 +29,9 @@ constructor(public navCtrl: NavController, public afs:AngularFirestore, public n
     console.log('ionViewDidLoad RepairNoticePage');
   }
 
+  ionViewWillEnter() {
+    this.global.changeMessage(false);
+  }
+  
 }
 
