a069d971348444c63ad81c00d1572d537ef40701
jjunyong <21300649@handong.edu>
2018-08-10 11:15:01 +0900

    투명한 카드로 수정

--- src/pages/home/stock-manage/item-detail/item-detail.ts
+++ src/pages/home/stock-manage/item-detail/item-detail.ts
@@ -38,7 +38,6 @@ export class ItemDetailPage {
   public changed_quantity: any;
 
   backgroundImage = "https://firebasestorage.googleapis.com/v0/b/prototype-d68e4.appspot.com/o/%EB%A9%94%EC%9D%B8%ED%8E%98%EC%9D%B4%EC%A7%802_%ED%88%AC%EB%AA%85.png?alt=media&token=b4bb27d8-9ce6-44b5-b979-a5d24c2401b2";
-  cardImage = "https://firebasestorage.googleapis.com/v0/b/prototype-d68e4.appspot.com/o/%EB%A9%94%EC%9D%B8%ED%8E%98%EC%9D%B4%EC%A7%802_%ED%88%AC%EB%AA%852.png?alt=media&token=78826653-cbd4-442d-9607-0b03983167b5"
 
   downloadURL
   uploadPercent
